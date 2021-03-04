64a6268067ed5e26af2bbe467aac87b4e1be9bc2
Aaron Van Curen <aaronvancuren@gmail.com>
2021-03-04 10:40:47 -0500

    Added Class Email

--- AgileProject_WebAPI/Providers/ApplicationOAuthProvider.cs
+++ AgileProject_WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using AgileProject_WebAPI.Models;
+using AgileProject_Data;
 
 namespace AgileProject_WebAPI.Providers
 {
